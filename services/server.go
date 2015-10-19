7142e333ed8b4681d8e1fe1e12a78c0c54b91a96
Oleg Dolya <oleg.dolya@gmail.com>
2015-10-19 13:46:29 +0300

    added Server service

---
+++ services/server.go
@@ -0,0 +1,91 @@
+package services
+
+import (
+	"fmt"
+	// "net"
+	"time"
+
+	"strconv"
+
+	"bitbucket.org/grengojbo/ads-core/config"
+	"bitbucket.org/grengojbo/ads-core/core"
+	// "bitbucket.org/grengojbo/ads-core/db"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx"
+	"github.com/nu7hatch/gouuid"
+	log "gopkg.in/inconshreveable/log15.v2"
+)
+
+type Server struct {
+	// Bus    *bus.Bus
+	Config  *config.Config
+	DB      *pgx.ConnPool
+	Release bool
+	r, s    *gin.Engine
+	log     log.Logger
+}
+
+// Start Web Server
+func (self *Server) Start() {
+	// logger := log.New()
+	self.log = log.New()
+	// mlog.Info("starting server service")
+
+	if self.Release {
+		gin.SetMode(gin.ReleaseMode)
+		self.r = gin.New()
+	} else {
+		self.r = gin.New()
+		self.r.Use(gin.Logger())
+	}
+	self.r.Use(gin.Recovery())
+
+	self.r.NoRoute(self.redirect)
+	self.r.GET("/ping", self.ping)
+
+	show := self.r.Group("show")
+	show.GET(":region_id/:umac/ping.js", self.showPing)
+
+	// go self.r.Run(fmt.Sprintf("%s:%d", self.Config.Host, self.Config.Port))
+	self.r.Run(fmt.Sprintf("%s:%d", self.Config.Host, self.Config.Port))
+}
+
+// Stop Web Server
+func (self *Server) Stop() {
+	// mlog.Info("server service stopped")
+	// nothing here
+}
+
+// Redirect no route
+func (self *Server) redirect(c *gin.Context) {
+	c.Redirect(301, "/")
+}
+
+// ping pong :)
+func (self *Server) ping(c *gin.Context) {
+	c.String(200, "pong")
+}
+
+func (self *Server) showPing(c *gin.Context) {
+	u4, err := uuid.NewV4()
+	if err != nil {
+		log.Error("Is not generate uuid", err)
+	}
+	sesUuid := fmt.Sprintf("%v", u4)
+
+	storeID, err := strconv.Atoi(c.Param("region_id"))
+	if err != nil {
+		self.log.Error("Region ID is not integer", storeID)
+	}
+
+	t := time.Now().UTC()
+	core.SaveShow(self.DB, t, sesUuid, storeID, c.Param("umac"), c.ClientIP(), c.Request.Header.Get("Accept-Language")[0:2], c.Request.Referer(), c.Request.UserAgent())
+
+	c.Header("cache-control", "priviate, max-age=0, no-cache")
+	c.Header("pragma", "no-cache")
+	c.Header("expires", "-1")
+	c.Header("Last-Modified", fmt.Sprintf("%v", t))
+	// c.Header("Date", t.Format(time.RFC1123))
+	c.Header("Content-Type", "text/javascript")
+	c.String(200, fmt.Sprintf("var uatv_me_uuid='%s';", sesUuid))
+}
